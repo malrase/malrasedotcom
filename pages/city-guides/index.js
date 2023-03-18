f10a6aa7c95c5bfd3da4946d0610a3dd72d7711a
Michael Lintorn-Catlin <malrase@gmail.com>
2023-03-18 13:58:12 -0400

    Update index.js

--- pages/city-guides/index.js
+++ pages/city-guides/index.js
@@ -12,8 +12,8 @@ export default function CityGuidesIndex() {
       <p>Hopefully this list will grow as I spend more time in various cities.</p>
       <ul>
         <li><Link href={`./city-guides/london`}>London</Link></li>
-        <li><Link href={`./city-guides/new-orleans`}>New Orleans</Link</li>
-        <li><Link href={`./city-guides/new-york`}>New York</Link</li>
+        <li><Link href={`./city-guides/new-orleans`}>New Orleans</Link></li>
+        <li><Link href={`./city-guides/new-york`}>New York</Link></li>
         <li><Link href={`./city-guides/london`}>Paris</Link></li>
         <li><Link href={`./city-guides/prague`}>Prague</Link></li>
       </ul>
