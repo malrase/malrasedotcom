674c5233825811a4cb093a4f2824360a594a8a11
Michael Lintorn-Catlin <malrase@gmail.com>
2023-04-24 16:14:31 -0400

    Updating current reading for April 2023

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,11 +7,13 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li>Code Breaker - Walter Issacson</li>
-        <li>The Idiot - Fyodor Dostoevsky</li>
+        <li><a href='https://www.goodreads.com/book/show/60092581-lessons'>Lessons</a> - Ian McEwan</li>
+        <li><a href='https://www.goodreads.com/book/show/58312013-other-terrors'>Other Terrors: An Inclusive Anthology</a> - Vince A. Liaguno, et al.</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
+        <li><a href='https://www.goodreads.com/book/show/58437521-the-candy-house'>The Candy House</a> - Jennifer Egan</li>
+        <li>Liberation Day - George Saunders</li>
         <li>Cloud Cuckoo Land - Anthony Doerr</li>
       </ul>
       </div>
