4cfa5764c25d7782f28eae0d55edce68561fa3f6
Michael Lintorn-Catlin <malrase@gmail.com>
2023-05-15 16:45:32 -0400

    Update index.js

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,14 +7,14 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/60092581-lessons'>Lessons</a> - Ian McEwan</li>
+        <li><a href='https://www.goodreads.com/book/show/61053829-i-have-some-questions-for-you'>I Have Some Questions for You</a> - Rebecca Makkai</li>
         <li><a href='https://www.goodreads.com/book/show/58312013-other-terrors'>Other Terrors: An Inclusive Anthology</a> - Vince A. Liaguno, et al.</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
+        <li><a href='https://www.goodreads.com/book/show/60092581-lessons'>Lessons</a> - Ian McEwan</li>
         <li><a href='https://www.goodreads.com/book/show/58437521-the-candy-house'>The Candy House</a> - Jennifer Egan</li>
-        <li>Liberation Day - George Saunders</li>
-        <li>Cloud Cuckoo Land - Anthony Doerr</li>
+        <li><a href='https://www.goodreads.com/en/book/show/60471573'>Liberation Day</a> - George Saunders</li>
       </ul>
       </div>
     </main>
