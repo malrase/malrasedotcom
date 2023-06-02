dacd9c22d6cfade67f3a3b870bc129b2877f6f1c
Michael Lintorn-Catlin <malrase@gmail.com>
2023-06-02 17:32:46 -0400

    Adding The Wager//The Man Who Died Twice

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,11 +7,12 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/61053829-i-have-some-questions-for-you'>I Have Some Questions for You</a> - Rebecca Makkai</li>
-        <li><a href='https://www.goodreads.com/book/show/58312013-other-terrors'>Other Terrors: An Inclusive Anthology</a> - Vince A. Liaguno, et al.</li>
+        <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
+        <li><a href='https://www.goodreads.com/book/show/55457493-the-man-who-died-twice'>The Man Who Died Twice</a> - Richard Osman</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
+        <li><a href='https://www.goodreads.com/book/show/61053829-i-have-some-questions-for-you'>I Have Some Questions for You</a> - Rebecca Makkai</li>
         <li><a href='https://www.goodreads.com/book/show/60092581-lessons'>Lessons</a> - Ian McEwan</li>
         <li><a href='https://www.goodreads.com/book/show/58437521-the-candy-house'>The Candy House</a> - Jennifer Egan</li>
         <li><a href='https://www.goodreads.com/en/book/show/60471573'>Liberation Day</a> - George Saunders</li>
