36864e73d4df2c0ab45474e0a7816192df80aa90
Michael Lintorn-Catlin <malrase@gmail.com>
2024-03-10 11:29:12 -0400

    Book update as of March 2024

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,16 +7,16 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/61190770-chain-gang-all-stars'>Chain-Gang All-Stars</a> - Nana Kwame Adjei-Brenyah</li>
-        <li><a href='https://www.goodreads.com/book/show/187020.Empire_Falls'>Empire Falls</a> - Richard Russo</li>
-      <li><a href='https://www.goodreads.com/book/show/61358638-poverty-by-america'>Poverty, by America</a> - Matthew Desmond</li>
+        <li><a href='https://www.goodreads.com/book/show/61724281-the-best-minds'>The Best Minds</a> - Jonathan Rosen</li>
+        <li><a href='https://www.goodreads.com/book/show/23129923-fifteen-dogs'>Fifteen Dogs</a> - Abdré Alexis</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
-                <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
-        <li><a href='https://www.goodreads.com/book/show/55457493-the-man-who-died-twice'>The Man Who Died Twice</a> - Richard Osman</li>
-        <li><a href='https://www.goodreads.com/book/show/58437521-the-candy-house'>The Candy House</a> - Jennifer Egan</li>
-        <li><a href='https://www.goodreads.com/en/book/show/60471573'>Liberation Day</a> - George Saunders</li>
+        <li><a href='https://www.goodreads.com/book/show/4980.Breakfast_of_Champions'>Breakfast of Champions</a> - Kurt Vonnegut</li>
+        <li><a href='https://www.goodreads.com/book/show/27071490-homegoing'>Homegoing</a> - Yaa Gyasi</li>
+        <li><a href='https://www.goodreads.com/book/show/75665931-the-maniac'>The MANIAC</a> - Benjamín Labatut</li>
+        <li><a href='https://www.goodreads.com/book/show/65215321-blackouts'>Blackouts</a> - Justin Torres</li>
+        <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
       </ul>
       </div>
     </main>
