8a1bcd9c9a56ee6555bc2434464e1cd61beee31d
Michael Lintorn-Catlin <malrase@gmail.com>
2024-04-15 09:16:54 -0500

    Books for Apr 2024

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,16 +7,22 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/61724281-the-best-minds'>The Best Minds</a> - Jonathan Rosen</li>
-        <li><a href='https://www.goodreads.com/book/show/23129923-fifteen-dogs'>Fifteen Dogs</a> - Abdré Alexis</li>
+        <li><a href='https://www.goodreads.com/book/show/23533039-ancillary-mercy'>Ancillary Mercy</a> - Ann Leckie</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/4980.Breakfast_of_Champions'>Breakfast of Champions</a> - Kurt Vonnegut</li>
-        <li><a href='https://www.goodreads.com/book/show/27071490-homegoing'>Homegoing</a> - Yaa Gyasi</li>
-        <li><a href='https://www.goodreads.com/book/show/75665931-the-maniac'>The MANIAC</a> - Benjamín Labatut</li>
-        <li><a href='https://www.goodreads.com/book/show/65215321-blackouts'>Blackouts</a> - Justin Torres</li>
-        <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
+        <li>Fiction</li>
+        <li><ul>
+          <li><a href='https://www.goodreads.com/book/show/60784757-birnam-wood'>Birnam Wood</a> - Eleanor Catton</li>
+          <li><a href='https://www.goodreads.com/book/show/4980.Breakfast_of_Champions'>Breakfast of Champions</a> - Kurt Vonnegut</li>
+          <li><a href='https://www.goodreads.com/book/show/27071490-homegoing'>Homegoing</a> - Yaa Gyasi</li>
+        </ul></li>
+        <li>Non-Fiction</li>
+        <li><ul>
+          <li><a href='https://www.goodreads.com/book/show/75665931-the-maniac'>The MANIAC</a> - Benjamín Labatut</li>
+          <li><a href='https://www.goodreads.com/book/show/65215321-blackouts'>Blackouts</a> - Justin Torres</li>
+          <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
+      </ul></li>
       </ul>
       </div>
     </main>
