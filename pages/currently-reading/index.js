64d47f1c9788da7383bcd9500f38bb304435e089
Michael Lintorn-Catlin <malrase@gmail.com>
2024-04-15 09:24:20 -0500

    Nested lists don't need the li wrapper

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -12,17 +12,17 @@ export default function CurrentlyReadingIndex() {
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
         <li>Fiction</li>
-        <li><ul>
+        <ul>
           <li><a href='https://www.goodreads.com/book/show/60784757-birnam-wood'>Birnam Wood</a> - Eleanor Catton</li>
           <li><a href='https://www.goodreads.com/book/show/4980.Breakfast_of_Champions'>Breakfast of Champions</a> - Kurt Vonnegut</li>
           <li><a href='https://www.goodreads.com/book/show/27071490-homegoing'>Homegoing</a> - Yaa Gyasi</li>
-        </ul></li>
+        </ul>
         <li>Non-Fiction</li>
-        <li><ul>
+        <ul>
           <li><a href='https://www.goodreads.com/book/show/75665931-the-maniac'>The MANIAC</a> - Benjamín Labatut</li>
           <li><a href='https://www.goodreads.com/book/show/65215321-blackouts'>Blackouts</a> - Justin Torres</li>
           <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
-      </ul></li>
+      </ul>
       </ul>
       </div>
     </main>
