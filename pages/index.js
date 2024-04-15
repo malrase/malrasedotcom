46ff8a5dbf3019bb0e7ed8f0d1d68a44fac975e4
Michael Lintorn-Catlin <malrase@gmail.com>
2024-04-15 09:13:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className="home">
         You have found the personal site for Michael Lintorn-Catlin.
-        He likes <Link href="https://instagram.com/malrase">making cocktails</Link>, <Link href="https://letterboxd.com/malrase/">watching horror movies</Link>, and <Link href="https://www.goodreads.com/user/show/6233721-michael-lintorn-catlin">reading</Link>. By day, he works on the Appointments product at Square. By night, he visits bars. He lives in New York City with his husband, Hampton. 
+        He likes <Link href="https://instagram.com/malrase">making cocktails</Link>, <Link href="https://letterboxd.com/malrase/">watching horror movies</Link>, and <Link href="https://www.goodreads.com/user/show/6233721-michael-lintorn-catlin">reading</Link>. By day, he works at Square as an engineering lead. By night, he visits bars. He lives in New York City with his husband, Hampton. 
       </main>
     </>
   )
