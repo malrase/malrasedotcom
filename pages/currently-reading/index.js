75c509adf2d232b0d7ebf62bc89a06e77c800567
Michael Lintorn-Catlin <malrase@gmail.com>
2024-07-18 15:51:30 +0100

    Books as of July 2024

--- pages/currently-reading/index.js
+++ pages/currently-reading/index.js
@@ -7,21 +7,22 @@ export default function CurrentlyReadingIndex() {
       <div className="content">
       <p>Here's what I'm currently reading:</p>
       <ul>
-        <li><a href='https://www.goodreads.com/book/show/23533039-ancillary-mercy'>Ancillary Mercy</a> - Ann Leckie</li>
+        <li><a href='https://www.goodreads.com/book/show/3711.White_Teeth'>White Teeth</a> - Zadie Smith</li>
       </ul>
       <p>Here are some books I've enjoyed recently:</p>
       <ul>
         <li>Fiction</li>
         <ul>
+          <li><a href='https://www.goodreads.com/book/show/62039166-the-bee-sting'>The Bee Sting</a> - Paul Murray</li>
           <li><a href='https://www.goodreads.com/book/show/60784757-birnam-wood'>Birnam Wood</a> - Eleanor Catton</li>
-          <li><a href='https://www.goodreads.com/book/show/4980.Breakfast_of_Champions'>Breakfast of Champions</a> - Kurt Vonnegut</li>
+          <li><a href='https://www.goodreads.com/book/show/11761.Underworlds'>Underworld</a> - Don Delillo</li>
           <li><a href='https://www.goodreads.com/book/show/27071490-homegoing'>Homegoing</a> - Yaa Gyasi</li>
         </ul>
         <li>Non-Fiction</li>
         <ul>
+          <li><a href='https://www.goodreads.com/book/show/59808605-strangers-to-ourselves'>Strangers to Ourselvesd</a> - Rachel Aviv</li>
           <li><a href='https://www.goodreads.com/book/show/75665931-the-maniac'>The MANIAC</a> - Benjamín Labatut</li>
           <li><a href='https://www.goodreads.com/book/show/65215321-blackouts'>Blackouts</a> - Justin Torres</li>
-          <li><a href='https://www.goodreads.com/en/book/show/61714633'>The Wager: A Tale of Shipwreck, Mutiny, and Murder</a> - David Grann</li>
       </ul>
       </ul>
       </div>
