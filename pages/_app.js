d8e614b3b5133921016cc4cf68e0e47cd00e22b7
Michael Lintorn-Catlin <michaell@squareup.com>
2025-07-19 12:33:02 -0500

    Updating the bold font

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Nunito" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
       </Head>
       <Header/>
       <Component {...pageProps}/>
