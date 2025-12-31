7679b7ebcf9cb3e950f8557e638baccc43962fc2
Michael Lintorn-Catlin <malrase@gmail.com>
2025-12-31 11:05:22 -0600

    Adding bio

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
         He likes <Link href="https://instagram.com/malrase">making cocktails</Link>, 
         <Link href="https://letterboxd.com/malrase/">watching horror movies</Link>, 
         and <Link href="https://app.thestorygraph.com/profile/malrase">reading</Link>. 
-        By day, he works at Square as an engineering lead. By night, he visits bars. 
-        He lives in New York City with his husband, Hampton. 
+        By day, he works at Block as an engineering lead. By night, he reads and visits bars. 
+        He splits his time between New York City and New Orleans with his husband, Hampton. 
       </main>
     </>
   )
